ba85aa4d599203f99053955ac1f092f40575a8fb
sumi-0011 <selina2000@naver.com>
2023-04-04 21:26:05 +0900

    feat: header layout create

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Wrapper>
+            <Image
+                src="/assets/images/GOATANI_logo.svg"
+                width={91}
+                height={31}
+                alt="GOATANI"
+            />
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.header`
+    background-color: ${({ theme }) => theme.colors.primary[100]};
+    height: 59px;
+    display: flex;
+    align-items: center;
+    padding: 0px 18px;
+`
+
+export default Header
