858edb80344e2c31fd123577ad5043795ee8bd7c
jeonbyeongmin <qudals7613@gmail.com>
2023-04-06 14:36:59 +0900

    fix: icon build error

--- src/components/icon/message-icon.tsx
+++ src/components/icon/message-icon.tsx
@@ -6,30 +6,30 @@ export function MessageIcon({ size = 20 }: IconType) {
       <path
         d='M6.35717 10.2857C6.75166 10.2857 7.07145 9.96589 7.07145 9.5714C7.07145 9.17691 6.75166 8.85712 6.35717 8.85712C5.96268 8.85712 5.64288 9.17691 5.64288 9.5714C5.64288 9.96589 5.96268 10.2857 6.35717 10.2857Z'
         stroke='currentColor'
-        stroke-width='1.42857'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.42857'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M11 10.2857C11.3945 10.2857 11.7143 9.96589 11.7143 9.5714C11.7143 9.17691 11.3945 8.85712 11 8.85712C10.6055 8.85712 10.2857 9.17691 10.2857 9.5714C10.2857 9.96589 10.6055 10.2857 11 10.2857Z'
         stroke='currentColor'
-        stroke-width='1.42857'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.42857'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M15.6429 10.2857C16.0374 10.2857 16.3572 9.96589 16.3572 9.5714C16.3572 9.17691 16.0374 8.85712 15.6429 8.85712C15.2484 8.85712 14.9286 9.17691 14.9286 9.5714C14.9286 9.96589 15.2484 10.2857 15.6429 10.2857Z'
         stroke='currentColor'
-        stroke-width='1.42857'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.42857'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M6.71429 18.1429L1 19.5714L2.42857 15.2857V2.42857C2.42857 2.04969 2.57908 1.68633 2.84699 1.41842C3.1149 1.15051 3.47826 1 3.85714 1H18.1429C18.5217 1 18.8851 1.15051 19.153 1.41842C19.4209 1.68633 19.5714 2.04969 19.5714 2.42857V16.7143C19.5714 17.0932 19.4209 17.4565 19.153 17.7244C18.8851 17.9923 18.5217 18.1429 18.1429 18.1429H6.71429Z'
         stroke='currentColor'
-        stroke-width='1.42857'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.42857'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
