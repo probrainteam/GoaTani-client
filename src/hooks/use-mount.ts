44b01992865e37043371b193cd9c79b1f7032a6c
jeonbyeongmin <qudals7613@gmail.com>
2023-04-10 23:47:27 +0900

    feat: add mandalart carousel

---
+++ src/hooks/use-mount.ts
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from 'react';
+
+function useMount() {
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    isMounted.current = true;
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  return isMounted;
+}
+
+export default useMount;
