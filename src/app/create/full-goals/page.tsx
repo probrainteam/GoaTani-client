61871c5f6909456dbdea587476a07d25ce53cab9
sumi-0011 <selina2000@naver.com>
2023-04-11 13:55:59 +0900

    feat: image download code create

--- src/app/create/full-goals/page.tsx
+++ src/app/create/full-goals/page.tsx
@@ -2,11 +2,33 @@
 
 import MandalartFull from '@/components/mandalart/mandalart-full';
 import { DUMMY_MANDALART_TOTAL } from '@/constants/mandalart';
+import { useRef } from 'react';
+import html2canvas from 'html2canvas';
 
 function FullGoalsPage() {
+  /* 캡처 영역 변수 선언 */
+  const captureArea = useRef<HTMLDivElement>(null);
+
+  /* 다운로드 버튼 함수 */
+  const onCaptureButtonClick = () => {
+    if (captureArea.current) {
+      html2canvas(captureArea.current).then((canvas) => {
+        const element = document.createElement('a');
+        element.href = canvas.toDataURL('image/png');
+        element.download = '2023_Mandalart.png';
+        element.click();
+      });
+    }
+  };
+
   return (
     <div>
-      <MandalartFull contents={DUMMY_MANDALART_TOTAL} theme='primary' />
+      <div ref={captureArea}>
+        <MandalartFull contents={DUMMY_MANDALART_TOTAL} theme='primary' />
+      </div>
+      <button onClick={onCaptureButtonClick} className='button bg-vivaMegenta text-white'>
+        DOWNLOAD
+      </button>
     </div>
   );
 }
