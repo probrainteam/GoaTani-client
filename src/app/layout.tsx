f445fea0757573dc342ab720fd07810ef4dbb8d6
jeonbyeongmin <qudals7613@gmail.com>
2023-04-12 19:50:32 +0900

    fix(lib): support server-side styled-component
    * `lib/registry.tsx`: 서버에서 styled-component 의 스타일을 삽입할 수 있도록 합니다.
    * `app/theme-wrapper.tsx`: styled-component의 theme-provider를 **client**에서 생성합니다.
    * `app/layout.tsx`: layout을 **server**에서 생성합니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
-import { ReactNode } from 'react';
-import RootStyleRegistry from '@/lib/RootStyleRegistry';
-
 import './globals.css';
+import { ReactNode } from 'react';
+
+import StyledComponentsRegistry from '@/lib/registry';
+import { ThemeWrapper } from '@/app/theme-wrapper';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='ko'>
       <body>
-        <RootStyleRegistry>{children}</RootStyleRegistry>
+        <StyledComponentsRegistry>
+          <ThemeWrapper>{children}</ThemeWrapper>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
