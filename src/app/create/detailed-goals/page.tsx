de15add5268174bc0cce28034e5b4d1945373810
sumi-0011 <selina2000@naver.com>
2023-04-14 15:17:37 +0900

    feat: detail page move click

--- src/app/create/detailed-goals/page.tsx
+++ src/app/create/detailed-goals/page.tsx
@@ -1,13 +1,68 @@
 'use client';
 
+import Heading from '@/app/create/heading';
 import { MandalartCarousel } from '@/components/mandalart';
-import { DUMMY_MANDALART_TOTAL } from '@/constants/mandalart';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import styled from 'styled-components';
 
+const DUMMY_KEY_GOALS = [
+  {
+    id: 'subNaN',
+    content: '몸 만들기',
+  },
+  {
+    id: 'subsubNaN1',
+    content: '돈 벌기',
+  },
+  {
+    id: 'subsubsubNaN11',
+    content: '취업',
+  },
+  {
+    id: 'subsubsubsubNaN111',
+    content: '취미',
+  },
+  {
+    id: 'subsubsubsubsubNaN1111',
+    content: '여행',
+  },
+  {
+    id: 'subsubsubsubsubsubNaN11111',
+    content: '책 읽기',
+  },
+  {
+    id: 'subsubsubsubsubsubsubNaN111111',
+    content: 'sadsadsad',
+  },
+  {
+    id: 'subsubsubsubsubsubsubsubNaN1111111',
+    content: 'sadasdsadsad',
+  },
+];
 export default function DetailedGoalsPage() {
+  const completeCount = 0;
+  const router = useRouter();
+
+  const handleCarouselClick = (id: number) => {
+    router.push(`/create/detailed-goals/${id}`);
+  };
+
+  const [contents, setContents] = useState(
+    DUMMY_KEY_GOALS.map((mainContent) => ({
+      mainContent,
+      subContents: [],
+    })),
+  );
+
   return (
     <Wrapper>
-      <MandalartCarousel contents={DUMMY_MANDALART_TOTAL} theme='primary' />
+      <Heading>
+        세부 목표를 정해주세요! <br />
+        오타니 되기 까지 {60 + completeCount * 5}%
+      </Heading>
+
+      <MandalartCarousel contents={contents} theme='primary' onClick={handleCarouselClick} />
     </Wrapper>
   );
 }
