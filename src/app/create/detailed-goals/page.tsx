5ba55eb0e0922fdcf5a7c4a1a4cabcec6674a520
sumi-0011 <selina2000@naver.com>
2023-04-15 00:20:11 +0900

    feat: key goal 저장 수정

--- src/app/create/detailed-goals/page.tsx
+++ src/app/create/detailed-goals/page.tsx
@@ -2,59 +2,30 @@
 
 import Heading from '@/app/create/heading';
 import { MandalartCarousel } from '@/components/mandalart';
+import { MandalartPartType } from '@/types/mandalart';
+import { getCreateStorage } from '@/utils/storage';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import styled from 'styled-components';
 
-const DUMMY_KEY_GOALS = [
-  {
-    id: 'subNaN',
-    content: '몸 만들기',
-  },
-  {
-    id: 'subsubNaN1',
-    content: '돈 벌기',
-  },
-  {
-    id: 'subsubsubNaN11',
-    content: '취업',
-  },
-  {
-    id: 'subsubsubsubNaN111',
-    content: '취미',
-  },
-  {
-    id: 'subsubsubsubsubNaN1111',
-    content: '여행',
-  },
-  {
-    id: 'subsubsubsubsubsubNaN11111',
-    content: '책 읽기',
-  },
-  {
-    id: 'subsubsubsubsubsubsubNaN111111',
-    content: 'sadsadsad',
-  },
-  {
-    id: 'subsubsubsubsubsubsubsubNaN1111111',
-    content: 'sadasdsadsad',
-  },
-];
-
 export default function DetailedGoalsPage() {
   const completeCount = 0;
   const router = useRouter();
+  const keyGoals = getCreateStorage('key-goal');
+
+  const [contents, setContents] = useState<MandalartPartType[]>(
+    keyGoals.map((content: string, idx: number) => ({
+      mainContent: {
+        id: String(idx),
+        content,
+      },
+      subContents: [],
+    })),
+  );
 
   const handleCarouselClick = (id: number) => {
     router.push(`/create/detailed-goals/${id}`);
   };
-
-  const [contents, setContents] = useState(
-    DUMMY_KEY_GOALS.map((mainContent) => ({
-      mainContent,
-      subContents: [],
-    })),
-  );
 
   return (
     <Wrapper>
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,11 +1,17 @@
+import { MandalartTileType } from '@/types/mandalart';
+
 const CREATE_STORAGE_KEY = 'goatani';
 
 type CreateKeyType = 'key-goal' | 'full-goal' | 'detailed-goals';
 
+interface BranchesType {
+  title: string;
+  details: string[];
+}
+
 const INIT_CREATE_STORAGE = {
   'full-goal': '',
-  'key-goal': [],
-  'detailed-goals': [],
+  'branches': [],
 };
 
 export const setCreateStorage = (key: CreateKeyType, value: string | any) => {
@@ -13,11 +19,27 @@ export const setCreateStorage = (key: CreateKeyType, value: string | any) => {
 
   const obj = item ? JSON.parse(item) : INIT_CREATE_STORAGE;
 
-  obj[key] = value;
+  if (key === 'full-goal') {
+    obj[key] = value;
+  }
 
+  if (key === 'key-goal') {
+    const newKeyGoalObj = value.map((item: MandalartTileType) => ({
+      title: item.content,
+      details: [],
+    }));
+    obj['branches'] = newKeyGoalObj;
+  }
   window.sessionStorage.setItem(CREATE_STORAGE_KEY, JSON.stringify(obj));
 };
 
-export const getCreateStorage = (key: string) => {
-  return key;
+export const getCreateStorage = (key?: string) => {
+  const item = window.sessionStorage.getItem(CREATE_STORAGE_KEY);
+  const obj = item ? JSON.parse(item) : INIT_CREATE_STORAGE;
+  if (!key) return obj;
+
+  if (key === 'key-goal') {
+    return obj['branches'].map((item: BranchesType) => item.title);
+  }
+  return obj[key];
 };
