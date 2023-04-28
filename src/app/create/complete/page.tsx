154487c00446ccbc4f8781932e981272c5a2c5d6
sumi-0011 <selina2000@naver.com>
2023-04-28 20:33:45 +0900

    feat: 만다라트 완성 버튼 UI 생성

--- src/app/create/complete/page.tsx
+++ src/app/create/complete/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 
+import Button from '@/components/button';
 import { MandalartCarousel } from '@/components/mandalart';
 import useDidMount from '@/hooks/use-did-mount';
 import { MandalartPartType } from '@/types/mandalart';
 import { BranchesType, CreateStorageType, getCreateStorage } from '@/utils/storage';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 function CreateCompletePage() {
   const [contents, setContents] = useState<MandalartPartType[]>([]);
+
+  const fullGoal = contents.length > 0 && contents[0].mainContent;
 
   const initSetting = () => {
     const data = getCreateStorage() as CreateStorageType;
@@ -29,9 +33,40 @@ function CreateCompletePage() {
 
   return (
     <div>
+      {/* TODO : 스켈레톤 추가 */}
+      <Heading>{fullGoal}</Heading>
       <MandalartCarousel contents={contents} theme='primary' isFullMandalart />
+
+      <ButtonWrapper>
+        <Button>피드에 공유하기</Button>
+
+        <Button variant='outline'>다운로드</Button>
+      </ButtonWrapper>
     </div>
   );
 }
+
+const Heading = styled.h1`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 22px;
+
+  text-align: center;
+
+  color: #000000;
+  text-decoration: underline;
+  text-underline-offset: 4px;
+
+  margin-top: 37px;
+  margin-bottom: 10px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 45px;
+  gap: 6px;
+`;
 
 export default CreateCompletePage;
